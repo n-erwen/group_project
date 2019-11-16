81c6c69246443492251fcce8fded5b3acb447712
Dalma <lipcsei.dalma@gmail.com>
2019-11-16 15:47:00 +0000

    changed about.html background, minor changes in page structure, added jquery sticky nav and scroll to section

--- app.js
+++ app.js
@@ -19,9 +19,12 @@ anime.timeline({loop: true})
 
 //OPENSTREETMAP
   var planes = [
-		["7C6B07",51.5669,0.1471],
-		["7C6B38",51.5073,0.1657],
-		["7C6CA1",51.5746,0.1308],
+		["The Parkland Walk Spriggan",51.5646,0.1047],
+		["Hyde Park",51.5073,0.1657],
+		["Bruce Castle Museum",51.5991,0.0754],
+    ["The Parkland Walk Spriggan",51.5646,0.1047],
+		["Hyde Park",51.5073,0.1657],
+		["Bruce Castle Museum",51.5991,0.0754],
 		];
 
         var map = L.map('map').setView([51.5074, 0.1277], 10);
@@ -38,3 +41,64 @@ anime.timeline({loop: true})
 				.bindPopup(planes[i][0])
 				.addTo(map);
 		}
+
+//Jquery waypoints: http://imakewebthings.com/waypoints
+$(document).ready(function() {
+  //Sticky Navigation
+  $('.js--section-featured').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  }, {
+    offset: '60px'
+  });
+
+  //scroll on buttons
+  $('.js--scroll-to-featured').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-featured').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-packages').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-packages').offset().top}, 1000);
+  });
+
+  //Scroll to section navi - https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+});
